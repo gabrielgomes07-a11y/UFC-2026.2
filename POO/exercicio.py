a307be91e6cd1c4589b5c817b8a6fd0d11f60c87
Angelo Gabriel <gabrielgomes07@alu.ufc.br>
2026-08-13 15:53:58 -0300

    Adicionando fahrenheit para celsius

--- POO/exercicio.py
+++ POO/exercicio.py
@@ -1,7 +1,23 @@
+# bloco de converão de c para f e vice versa
+
 def convertgraus(value1):
     fahr = (value1 * 1.8) + 32
     return fahr
 
 
-celsius = float(input('Digite os Graus celsius: '))
-print(convertgraus(celsius))
+def convertgraus1(value2):
+    cels = (value2 - 32) / 1.8
+    return cels
+
+
+# bloco de verificação de entrada
+tipo_grau = str(input('Qual a grandeza: '))
+
+if tipo_grau == 'C':
+
+    graus = float(input('Digite os graus celsius: '))
+    print('{}°F'.format(convertgraus(graus)))
+
+else:
+    graus = float(input('Digite os graus fahrenheit: '))
+    print('{}°C'.format(convertgraus1(graus)))
